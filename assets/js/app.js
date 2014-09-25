93cf897d3aaaf2230f30f268237161a142b15224
Richard <richard.be.jamin@gmail.com>
2014-09-25 18:19:09 -0500

    more work

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,15 +61,15 @@ $(function(){
 
 	$(document).on('click', '.add', function(){
 		var options_block = $(this).next('.options');
+		// options_block.toggleClass('active');
 		if( $(this).hasClass('active') ){
-			options_block.animate({'height': 0, 'width' : 0 }, { queue: false, duration: 400 });
+			options_block.animate({'height': 0, 'width' : 0 }, { queue: false, duration: 550 });
 			$(this).removeClass('active');
-			options_block.fadeOut(400);
+			options_block.fadeOut(550);
 		} else {
 			$(this).addClass('active');
-			options_block.fadeIn(400);
-			options_block.animate({'height': $('.column').height(), 'width' : '100%' }, { queue: false, duration: 400 });
-			
+			options_block.fadeIn(550);
+			options_block.animate({'height': $('.column').height(), 'width' : '100%' }, { queue: false, duration: 550 });
 		}
 
 	});
